7ba2b75f86f9cfd157cb5eddbdfa66e18fd8c638
brandontaylor1 <brandon@thecreativekingdomco.com>
2025-03-12 09:37:50 -0500

    Install ESLint and Prettier configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const Home = () => <h1 className="text-4xl text-violet-700 font-black">Welcome to the World of Next.Js!</h1>
+const Home = () => {
+
+  return (
+      <h1 className="text-4xl text-violet-700 font-black">Welcome to the World of Next.Js!</h1>
+  )
+}
+
 
 
 export default Home;
